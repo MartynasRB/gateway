958215fe051c294cae09f5f0b55216f431d72798
MartynasRB <115002251+MartynasRB@users.noreply.github.com>
2023-11-28 14:52:12 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
